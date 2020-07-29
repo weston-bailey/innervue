7225836a81bf910df23a501f3c1212c335682233
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2020-07-28 21:34:24 -0700

    analysis logic in progress

--- testApi4.js
+++ testApi4.js
@@ -33,7 +33,7 @@ const test = function (req, res) {
 test();
 
 // The text to analyze
-const text = "Hello, my name is Inigo Montoya. You killed my father. Prepare to die.";
+const text = "An organization I was employed for was undergoing budget cuts due to the recession. It was a time of big change because they were restructuring the management team, closing locations, eliminating positions and moving staff around. I think what made it the most challenging was that management was not very communicative and I often heard about a new decision from a colleague instead of from the director or my manager. I was personally impacted because  I did not know if I was going to keep my job or if I was going to be moved to a different location and end up with a different schedule. In order to adapt to the change, I reached out to the communications director to provide feedback on my experience with receiving information in an appropriate and timely manner. The communications director was receptive to this feedback and started sending out a weekly newsletter to all employees regarding the discussions, plans and decisions being made at the executive level. On a personal level, I prepared for possible layoff by being proactive with my job search and this helped me see that I did have options if I ended up losing my job. ";
 
 googleCloud(text);
 
@@ -85,7 +85,52 @@ async function googleCloud(text) {
         }
 
         // TODO: Format anaylsis from APIs
+        //Detect if there is enough content to proceed with analysis
+        let responseLength = analyzeTone.utterances_tone.utterance_id
+        if (responseLength < 4) {
+            return "We suggest you provide a longer response to fully answer the interview question and so we can provide adequate feedback."
+        } else {
+            return "Here's the summary analysis of your response:"
+        } 
+        //Overall sentiment of response
+        let score = payload.analyzeSentiment[0].documentSentiment.score
+        let magnitude = payload.analyzeSentiment[0].documentSentiment.magnitude
 
+              if (payload.analyzeSentiment[0].documentSentiment.score >= 0.5 && payload.analyzeSentiment[0].documentSentiment.magnitude >= 3.0) {
+                return "You response reflects a clearly positive sentiment. This will appeal to interviewers!"
+              } else if (payload.analyzeSentiment[0].documentSentiment.score = 0.0 && payload.analyzeSentiment[0].documentSentiment.magnitude === 0) { 
+                  return "Your response is looking good, try modifying it some more to make it more impactful."
+              } else if (payload.analyzeSentiment[0].documentSentiment.score <= -0.05 && payload.analyzeSentiment[0].documentSentiment.magnitude === 4.0) {
+              return  "Your response reflects a negative sentiment. We suggest modifying your response to communicate more effectively." 
+              } 
+
+        //Detect if there is negative entity sentiment
+            //target mentions in 
+            analyzeEntitySentiment[0].entities.forEach(mention => {
+              if(mention.sentiment.score < -.5){
+                console.log('less than -.5')
+                console.log(`The response reflects a negative sentiment towards ${mention.name}. We suggest modifying your word choices to neutralize how this comes across.`)
+                // route should return json with message
+              }
+            })
+        //Print emotions detected:
+        payload.analyzeTone.utterances_tone.forEach(utterance => {
+          if(utterance.tones.length > 0){
+            // add found tones to database and send to client
+            let foundTones = []
+            utterance.tones.forEach(tone => {
+              foundTones.push(tone.tone_id);
+            })
+            console.log(foundTones)
+            console.log( `The emotion(s) we detected: ${tone_name}. Do they properly reflect what you want your interviewers to know?`)
+          } else {
+            // no tones found!
+            console.log('no tones!')
+          }
+        })
+        
+        
+       
         // TODO: Write anaylsis/question to database
 
         // TODO send analysis/question to client
