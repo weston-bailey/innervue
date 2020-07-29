e5b9330f20f949fc92f88ac6c16dea8b39001743
Weston Bailey <void@MBP.local>
2020-07-29 02:31:38 -0700

    client getting the data it needs to render

--- client/src/content/pages/GetFeedback.js
+++ client/src/content/pages/GetFeedback.js
@@ -5,6 +5,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import { Box } from '@material-ui/core'
 import QuestionSelector from '../components/QuestionSelector'
 import { set } from 'd3';
+import questions from '../Questions';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -38,10 +39,22 @@ const useStyles = makeStyles(theme => ({
 
 
 const GetFeedback = (props) => {
-        // pass down as properties to QuestionSelector
-    const [selectedQuestion, setSelectedQuestion] = useState("")
-    const [selectedCategory, setSelectedCategory] = useState("")
-    const [analysis, setAnalysis] = useState(false)
+      // pass down as properties to QuestionSelector
+    const [selectedQuestion, setSelectedQuestion] = useState("");
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [analysis, setAnalysis] = useState(false);
+    // question object with analysis in it sent from server
+    const [question, setQuestion] = useState({
+      category: '',
+      content: '',
+      answer: '',
+      analysis: {
+        negetiveMentions: [],
+        overallScore: '',
+        overallMagnitude: '',
+        overallFeedback: ''
+      }
+    });
  
     const handleQuestionClick = (event) => {
         setSelectedQuestion(event.target.value)
@@ -52,35 +65,54 @@ const GetFeedback = (props) => {
     const classes = useStyles()
 
     const gettingFeedback = (
-        <Grid container spacing={6}>
-            <Grid item xs={12}></Grid>
-            <Grid item xs={12}>
-             <Box className={classes.banner}>
-                <div className="feedback-instructions">
-                <h1>feedback</h1>
-                <Grid item xs={4}>
-                <p>Talk or type in the text box and submit to get instant feedback on your response!</p>
-                </Grid> 
-                </div>
-            <Grid container spacing={6}>
-                <Grid item xs={6}>
-                    <FeedbackForm 
-                    className={classes.feedback}
-                    selectedQuestion={selectedQuestion}
-                    selectedCategory={selectedCategory}
-                    setAnalysis={setAnalysis}
-                    />
-                </Grid>
-                <Grid item xs={5}>
-                <QuestionSelector handleQuestionClick={handleQuestionClick}/>
-            </Grid>  
-            </Grid>
-            </Box>
-        </Grid>
-    </Grid>)
+          <Grid container spacing={6}>
+              <Grid item xs={12}></Grid>
+              <Grid item xs={12}>
+              <Box className={classes.banner}>
+                  <div className="feedback-instructions">
+                  <h1>feedback</h1>
+                  <Grid item xs={4}>
+                  <p>Talk or type in the text box and submit to get instant feedback on your response!</p>
+                  </Grid> 
+                  </div>
+              <Grid container spacing={6}>
+                  <Grid item xs={6}>
+                      <FeedbackForm 
+                      className={classes.feedback}
+                      selectedQuestion={selectedQuestion}
+                      selectedCategory={selectedCategory}
+                      setAnalysis={setAnalysis}
+                      setQuestion={setQuestion}
+                      />
+                  </Grid>
+                  <Grid item xs={5}>
+                  <QuestionSelector handleQuestionClick={handleQuestionClick}/>
+              </Grid>  
+              </Grid>
+              </Box>
+          </Grid>
+      </Grid>
+    )
+    console.log('question:', question);
+
+    // map the negetive mentions into something pretty -- this array can be empty sometimes (nothing was mentioned negetively)
+    question.analysis.negetiveMentions.map( negativeMention => {
+      return(
+        <div>
+          {negativeMention} was mentioned negetively mentioned 
+        </div>
+      )
+    })
 
     const gettingAnalysis = (
-        <div>hello</div>
+        <div>
+          <p>category: {question.category}</p>
+          <p>question: {question.content}</p>
+          <p>response: {question.answer}</p>
+          <p>over all score: {question.analysis.overallMagnitude} {question.analysis.overallScore} </p>
+          <p>feedback: {question.analysis.overallFeedback}</p>
+          <p>{question.analysis.negetiveMentions}</p>
+        </div>
     )
 
     return (
